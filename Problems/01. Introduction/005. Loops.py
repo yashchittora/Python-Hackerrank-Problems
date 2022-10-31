e72f8135c8d766559c9ca4e445670807a8a99d08
Yash Chittora <yashchittora.code@gmail.com>
2022-10-31 21:30:28 +0530

    Add 005

---
+++ Problems/01. Introduction/005. Loops.py
@@ -0,0 +1,7 @@
+# Problem Link : https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+n = int(input())
+    
+    if n>=1 and n<=20 :
+        for i in range(n):
+            print(i**2)
