a9c84a8094074ea0d73ad8ca2a2651e43c1603db
Yash Chittora <yashchittora.code@gmail.com>
2022-12-10 17:11:32 +0530

    Improve 002

--- Problems/01. Introduction/002. Python If-Else.py
+++ Problems/01. Introduction/002. Python If-Else.py
@@ -3,13 +3,13 @@
 n = int(input().strip())
 
 if n%2!=0 and 1<= n <=100 :
-	print("Weird")
-	
-if n%2==0 and 2<= n <=5 :
-	print("Not Weird")
-	
-if n%2==0 and 6<= n <= 20:
-	print("Weird")
-	
-if n%2==0 and n>20: 
-	print("Not Weird")
+    print("Weird")
+    
+elif n%2==0 and 2<= n <=5 :
+    print("Not Weird")
+    
+elif n%2==0 and 6<= n <= 20:
+    print("Weird")
+    x
+elif n%2==0 and n>20: 
+    print("Not Weird")
